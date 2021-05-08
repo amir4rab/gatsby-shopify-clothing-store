1e3b31952ed1835281cadcc7437dd7b829eb99e7
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-05-08 13:01:00 +0430

    first init

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
+module.exports = {
+  siteMetadata: {
+    title: "gatsby-shopify clothing store",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        shopName: process.env.SHOPIFY_SHOP_NAME,
+        accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
