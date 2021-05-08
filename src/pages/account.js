ccc946de0865cea8b0b6df23e253577b4b23d229
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-05-08 16:44:02 +0430

    added pages

---
+++ src/pages/account.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../layout/layout.component';
+
+const AccountPage = () => {
+    return (
+        <Layout>
+            <div>
+                Account
+            </div>
+        </Layout>
+    );
+};
+
+export default AccountPage;
---
+++ src/pages/cart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../layout/layout.component';
+
+const CartPage = () => {
+    return (
+        <Layout>
+            <div>
+                Cart
+            </div>
+        </Layout>
+    );
+};
+
+export default CartPage;
