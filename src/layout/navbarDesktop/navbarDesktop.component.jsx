611ca0e9f01fc54d44f3bdd00b936d6b184bf860
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-05-09 07:15:42 +0430

    navbarDesktop compeleted

--- src/layout/navbarDesktop/navbarDesktop.component.jsx
+++ src/layout/navbarDesktop/navbarDesktop.component.jsx
@@ -8,13 +8,16 @@ const NavbarDesktop = () => {
     return (
         <div className="navbarDesktop">
             <Link to="/" className="link" activeClassName="active">
-                <StaticImage alt="home" className="innerImg" placeholder="tracedSVG" src="./../../images/icons/pink/home.svg"/>
+                <StaticImage alt="home-active-link" className="innerImg_active" placeholder="tracedSVG" src="./../../images/icons/pink/home.svg"/>
+                <StaticImage alt="home" className="innerImg" placeholder="tracedSVG" src="./../../images/icons/home.svg"/>
             </Link>
             <Link to="/account" className="link" activeClassName="active">
-                <StaticImage alt="account" className="innerImg" placeholder="tracedSVG" src="./../../images/icons/pink/account.svg"/>
+                <StaticImage alt="account-active-link" className="innerImg_active" placeholder="tracedSVG" src="./../../images/icons/pink/account.svg"/>
+                <StaticImage alt="account" className="innerImg" placeholder="tracedSVG" src="./../../images/icons/account.svg"/>
             </Link>
             <Link to="/cart" className="link" activeClassName="active">
-                <StaticImage alt="cart" className="innerImg" placeholder="tracedSVG" src="./../../images/icons/pink/shopping_cart.svg"/>
+                <StaticImage alt="cart-active-link" className="innerImg_active" placeholder="tracedSVG" src="./../../images/icons/pink/shopping_cart.svg"/>
+                <StaticImage alt="cart" className="innerImg" placeholder="tracedSVG" src="./../../images/icons/shopping_cart.svg"/>
             </Link>
         </div>
     );
