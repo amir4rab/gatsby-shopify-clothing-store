c81884c09126807889424f43c6290c7a4f1bc98f
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-05-10 16:08:23 +0430

    searchInput updated

--- src/components/inputs/searchInput/searchInput.component.jsx
+++ src/components/inputs/searchInput/searchInput.component.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import { StaticImage } from 'gatsby-plugin-image';
+import { navigate } from 'gatsby';
 
+import searchIcon from '../../../images/icons/search.svg';
 import './searchInput.scss';
 
-function SearchInput() {
+const SearchInput = ({ eventFn = null, expanded = false, defaultValue = null }) => {
+
+    console.log(defaultValue);
+
+    const inputEventHandler = (e) => {
+        console.log(eventFn === null, e.code === 'Enter')
+        if(eventFn === null){
+            if( e.code === 'Enter' ) navigate(`/search?n=${e.target.value}`)
+        } else {
+            eventFn(e.target.value);     
+        }
+    }
+
     return (
-        <div className="searchInput">
-            <input className="input" type="text"/>
-            <StaticImage placeholder="tracedSVG"  className="icon" src="../../../images/icons/search.svg" alt="search" />
+        <div className={[
+                "searchInput",
+                expanded === false ? '' : 'expanded'
+            ].join(' ')}>
+            { 
+                defaultValue === null ?
+                <input onKeyDown={ inputEventHandler } className="input" type="text"/>
+                :
+                <input value={defaultValue} onChange={ inputEventHandler } className="input" type="text"/>
+
+            }
+            <img className="icon" src={searchIcon} alt="search" />
         </div>
     );
 };
