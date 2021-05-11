df4dd1ca3d8e0397165239d5d05824e55ad8e872
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-05-11 10:35:38 +0430

    ProductDisplayerComponent added

---
+++ src/components/productDisplayer/productDisplayer.component.jsx
@@ -0,0 +1,60 @@
+import React, { memo } from 'react';
+import { GatsbyImage } from 'gatsby-plugin-image';
+
+import * as classes from './productDisplayer.module.scss';
+
+const ProductDisplayerComponent = memo(({ dataArr }) => {
+    console.log(dataArr.length);
+
+    return (
+        <div className={ classes.productDisplayer }>
+            <div className={ classes.wrapper }>
+                { dataArr.map( item => (
+                    <div className={ classes.item } key={ item.shopifyId }>
+                        <div className={ classes.image }>
+                            <GatsbyImage alt={item.title} image={ item.images[0].localFile.childImageSharp.gatsbyImageData } />
+                        </div>
+                        <div className={ classes.details }>
+                            <p className={ classes.title }>
+                                { 
+                                    item.title.length > 17 ?
+                                    `${item.title.slice(0,14)}...` 
+                                    : 
+                                    item.title
+                                }
+                            </p>
+                            <p className={ classes.price }>€{ item.priceRange.minVariantPrice.amount }</p>
+                        </div>
+                    </div>
+                )) }
+            </div>
+        </div>
+    );
+},
+(pervProps, nextProps) => {
+    console.log(pervProps.dataArr.length, nextProps.dataArr.length);
+    if( pervProps.dataArr.length === nextProps.dataArr.length ) {
+        // console.log('block 1');
+        return true;
+    };
+    
+    if( nextProps.dataArr.length === 0 || pervProps.dataArr.length === 0 ) {
+        // console.log('block 1');
+        return false;
+    }; 
+
+    let loopResult = true;
+    pervProps.dataArr.every((item, i)=>{
+        if(item.title !== nextProps.dataArr[i].title){
+            // console.log('block 2')
+            loopResult = false;
+            return false;
+        }
+        return true;
+    });
+
+    // console.log(`loopResult: ${loopResult}`)
+    return loopResult;
+});
+
+export default ProductDisplayerComponent;
