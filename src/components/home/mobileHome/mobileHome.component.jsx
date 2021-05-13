af736ad9104deebe58b57de810003cdd7382b464
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-05-13 18:39:52 +0430

    MobileHome functionality added

--- src/components/home/mobileHome/mobileHome.component.jsx
+++ src/components/home/mobileHome/mobileHome.component.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
-// import { StaticImage } from 'gatsby-plugin-image';
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 import SearchInput from '../../inputs/searchInput/searchInput.component';
 import favoriteIcon from '../../../images/icons/favorite.svg';
+import CardSliderComponent from '../../cardSlider/cardSlider.component';
 
 import * as classes from './mobileHome.module.scss';
 
+
 const MobileHome = () => {
+    const query = useStaticQuery(graphql`
+        query getColletionsForMobileHomeComponent {
+            allShopifyCollection {
+                nodes {
+                    title
+                    image {
+                        localFile {
+                            childImageSharp {
+                                gatsbyImageData
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    console.log(query);
+
     return (
         <div className={ classes.mobileHome }>
             <div className={ classes.header }>
@@ -15,6 +35,9 @@ const MobileHome = () => {
                 <Link className={ classes.imageLink } to='/favorites'>
                     <img src={favoriteIcon} alt="favorate" />
                 </Link>
+            </div>
+            <div>
+                <CardSliderComponent dataArr={query.allShopifyCollection.nodes} />
             </div>
         </div>
     );
