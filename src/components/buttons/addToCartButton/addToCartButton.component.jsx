bbfe27ab32a5bd114ffc59f55bbe0048407fadf3
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-05-14 18:19:05 +0430

    button added

---
+++ src/components/buttons/addToCartButton/addToCartButton.component.jsx
@@ -0,0 +1,18 @@
+import React, { memo } from 'react';
+
+import addShoppingCartIcon from '../../../images/icons/black/add_shopping_cart.svg';
+
+import * as classes from './addToCartButton.module.scss'
+
+const AddToCartButton = memo(() => {
+    return (
+        <button className={ classes.addToCartButton }>
+            <p className={ classes.text }>
+                Add to Cart
+            </p>
+            <img className={ classes.icon } src={addShoppingCartIcon} alt="Add to shopping cart"/>
+        </button>
+    );
+});
+
+export default AddToCartButton;
