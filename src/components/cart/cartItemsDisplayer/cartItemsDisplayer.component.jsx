2e418539f43b896c054d2212d93bef989a9950fb
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-05-18 18:19:14 +0430

    desgin fixes

--- src/components/cart/cartItemsDisplayer/cartItemsDisplayer.component.jsx
+++ src/components/cart/cartItemsDisplayer/cartItemsDisplayer.component.jsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 import CartItemComponent from './cartItem/cartItem.component';
+import { Link } from 'gatsby';
 
 import * as classes from './cartItemsDisplayer.module.scss';
 
@@ -9,15 +10,24 @@ const CartItemsDisplayerComponent = ({ dataArr, removeItemFromCartFn }) => {
         removeItemFromCartFn(shopifyId)
     },[removeItemFromCartFn])
 
-    console.log(dataArr);
+    console.log(dataArr.length);
     return (
         <div className={ classes.cartItemsDisplayer }>
             {
-                dataArr.map(({ data, ammount }) => (
-                    <div className={ classes.item }  key={ data.shopifyId }>
-                        <CartItemComponent data={ data } ammount={ ammount } removeFn={removeFn} />
-                    </div>
-                ))
+                dataArr.length === 0 ?
+                <div className={ classes.emptyCart }>
+                    Your cart is empty go to <Link to="/">Home</Link> to add items to it!
+                </div>
+                :
+                <div>
+                    {
+                        dataArr.map(({ data, ammount }) => (
+                            <div className={ classes.item }  key={ data.shopifyId }>
+                                <CartItemComponent data={ data } ammount={ ammount } removeFn={removeFn} />
+                            </div>
+                        ))
+                    }
+                </div>
             }
         </div>
     );
