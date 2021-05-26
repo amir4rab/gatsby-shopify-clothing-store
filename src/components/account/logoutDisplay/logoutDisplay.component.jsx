ff5f699b60805712e75ecc8fbda23d5fea4388ac
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-05-26 15:46:13 +0430

    logout display added

---
+++ src/components/account/logoutDisplay/logoutDisplay.component.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+
+import SignInComponent from './signIn/signIn.component';
+import SignUpComponent from './signUp/signUp.component';
+import { 
+    signIn,
+    pSignIn,
+    signUp,
+    pSignUp,
+} from '../../../utilities/firebase/functions/functions';
+
+import * as classes from './logoutDisplay.module.scss';
+import LoadingDisplayComponent from '../../loadingDisplay/loadingDisplay.component';
+
+
+const LogoutDisplayComponent = ({ setUserData }) => {
+    const [ isNewUser, setIsNewUser ] = useState(true);
+    const [ isPersistent ] = useState(true);
+    const [ isLoading, setIsLoading ] = useState(false);
+    
+    const [ signinErros, setSigninErrors ] = useState(null);
+    const submitSingin = ( email, password ) => {
+        setIsLoading(true);
+        setSigninErrors(null);
+
+        if(isPersistent){
+            pSignIn(email, password)
+                .then(res => {
+                    setUserData(res);
+                    setIsLoading(false);
+                })
+                .catch(err => {
+                    setSigninErrors(err.message);
+                    setIsLoading(false);
+                })
+        } else {
+            signIn(email, password)
+                .then(res => {
+                    setUserData(res);
+                    setIsLoading(false);
+                })
+                .catch(err => {
+                    setSigninErrors(err.message);
+                    setIsLoading(false);
+                })
+        }
+    }
+
+    const [ signupErros, setSignupErrors ] = useState(null);
+    const submitSingup = ( email, password, rePassword ) => {
+        setIsLoading(true);
+        setSignupErrors(null);
+
+        if ( password !== rePassword ) {
+            setSignupErrors('Password repeat is wrong!');
+            setIsLoading(false);
+            return;
+        }
+
+        if(isPersistent){
+            pSignUp(email, password)
+                .then(res => {
+                    setUserData(res);
+                    setIsLoading(false);
+                })
+                .catch(err => {
+                    setSignupErrors(err.message);
+                    setIsLoading(false);
+                })
+        } else {
+            signUp(email, password)
+                .then(res => {
+                    setUserData(res);
+                    setIsLoading(false);
+                })
+                .catch(err => {
+                    setSignupErrors(err.message);
+                    setIsLoading(false);
+                })
+        }
+    }
+
+    return (
+        <div className={ classes.logoutDisplay }>
+            {
+                isLoading ?
+                <LoadingDisplayComponent /> :
+                <div>
+                    <h2 className={ classes.title }>
+                        Signin
+                    </h2>
+                    <div className={ classes.signinOptions }>
+                        <h3 className={ classes.title }>
+                            signin with
+                        </h3>
+                        <button className={ classes.signInWithGoogle }>
+                            signin with Google
+                        </button>
+                    </div>
+                    <div className={ classes.divider }>
+                        or
+                    </div>
+                    <div className={ classes.inputMethods }>
+                        <div
+                            className={ classes.toggler }>
+                            <button
+                                onClick={ _ => setIsNewUser(!isNewUser) }>
+                                {
+                                    `switch to ${isNewUser ? 'Signup' : 'Signin'}`
+                                }
+                            </button>
+                        </div>
+                        <div
+                            className={ classes.methodCompWrapper }
+                        >
+                            {/* { */}
+                                // isNewUser ? 
+                                <SignInComponent submitFn={ submitSingin } errors={ signinErros } /> 
+                                // : 
+                                <SignUpComponent submitFn={ submitSingup } errors={ signupErros } />
+                            {/* } */}
+                        </div>
+                    </div>
+                </div>
+            }
+        </div>
+    );
+};
+
+export default LogoutDisplayComponent;
