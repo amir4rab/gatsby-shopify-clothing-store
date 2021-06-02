eb3651d48a74d13b064f8655fad57f79828f5ba1
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-06-02 17:46:54 +0430

    favorates component completed

--- src/components/favorites/favorites.component.jsx
+++ src/components/favorites/favorites.component.jsx
@@ -1,57 +1,72 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef , useState, useCallback } from 'react';
 
+import * as classes from './favorites.module.scss';
+
+import GobackButton from '../buttons/gobackButton/gobackButton.component';
 import {
     getData,
     getUserPromise,
     getUser,
-    setData,
-    removeData,
 } from './../../utilities/firebase/functions/functions';
+import ProductDisplayerComponent from '../productDisplayer/productDisplayer.component';
+import LoadingDisplayComponent from '../loadingDisplay/loadingDisplay.component';
+
+
+const filterItems = (baseArr, filters) => {
+    const filterdArr = [];
+
+    filters.forEach(item => {
+        baseArr.forEach((element) => {
+            if(element.shopifyId === item) filterdArr.push(element);
+        });
+    })
+
+    return filterdArr;
+}
 
 const FavoritesComponent = ({ dataArr }) => {
+    let isMounted = useRef(true);
+    const [ filterdArr, setFilterdArr ] = useState([]);
+    const [ isLoading, setIsLoading ] = useState(true);
+
+    const getT = useCallback(() => {
+        getData('favorites').then(res => {
+            if(!isMounted) return;
+
+            setFilterdArr(filterItems(dataArr, Object.keys(res)));
+            setIsLoading(false);
+        });
+    },[dataArr]);
 
     useEffect(() => {
-        if( getUser() !== null ) return;
-        console.log('not retuned!');
-        getUserPromise().then(res => {
-            console.log(false);
+        if( getUser() !== null ) {
+            getT();
+        };
+
+        
+        getUserPromise().then( res => {
+            if (res === null) return;
+
+            getT();
         });
-    }, []);
-
-    const setT = () => {
-        setData('test1', 'asdfghjk').then(res => console.log(res));
-    }
-    const getT = () => {
-        getData('test1').then(res => console.log(res));
-    }
-    const removeT = () => {
-        removeData('test1').then(res => console.log(res));
-    }
-
-    // const getdata = () => {
-    //     getData().then(res => console.log(res));
-    // }
-    // getdata();
-
-    // console.log(dataArr);
+        return () => { isMounted.current = false }
+    }, [getT]);
 
     return (
-        <div>
-            <button 
-                style={{ color:'black' }}
-                onClick={ setT }>
-                setT
-            </button>
-            <button 
-                style={{ color:'black' }}
-                onClick={ getT }>
-                getT
-            </button>
-            <button 
-                style={{ color:'black' }}
-                onClick={ removeT }>
-                removeT
-            </button>
+        <div className={ classes.favorites }>
+            <div className={ classes.title }>
+                <GobackButton />
+                <h1>
+                    Favorites
+                </h1>
+            </div>
+            <div className={ classes.content }>
+                {
+                    isLoading ? 
+                    <LoadingDisplayComponent /> :
+                    <ProductDisplayerComponent dataArr={ filterdArr } />
+                }
+            </div>
         </div>
     );
 };
