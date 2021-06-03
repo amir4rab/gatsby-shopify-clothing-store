8a1fdadcb8ed3e2ee414859f4be725e0c374e3be
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-06-04 01:41:54 +0430

    about page and component added

---
+++ src/components/about/about.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import GobackButton from '../buttons/gobackButton/gobackButton.component';
+
+import * as classes from './about.module.scss';
+
+function AboutComponent() {
+    return (
+        <div className={ classes.about }>
+            <div className={ classes.header }>
+                <GobackButton />
+                <h1>
+                    About
+                </h1>
+            </div>
+            <div className={ classes.content }>
+                <p>
+                    developed with 💙 by <a href="https://amir4rab.com" target='_'>amir4rab</a>.
+                </p>
+                <p>
+                    feel free to check the websites code on <a href="https://github.com/amir4rab/gatsby-shopify-clothing-store" target='_'>Github</a>.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default AboutComponent;
---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AboutComponent from '../components/about/about.component';
+
+function AboutPage() {
+    return <AboutComponent />;
+};
+
+export default AboutPage;
