bd7a478a279fcc9d76cb43787ab92c719a01de2f
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-06-08 13:45:45 +0430

    address page added

---
+++ src/pages/checkout/address.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AddressPickerComponent from '../../components/addressPicker/addressPicker.component';
+
+function AddressPage() {
+    return <AddressPickerComponent />;
+};
+
+export default AddressPage;
