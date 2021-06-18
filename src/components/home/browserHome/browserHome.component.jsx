d697a1f7e50c9c36710048d5d4975f19fefc249f
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-06-18 20:25:48 +0430

    browserHome added

--- src/components/home/browserHome/browserHome.component.jsx
+++ src/components/home/browserHome/browserHome.component.jsx
@@ -1,20 +1,76 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { useStaticQuery, graphql, Link } from 'gatsby';
 
-import { connect } from 'react-redux';
+import * as classes from './browserHome.module.scss';
+import likeIcon from './../../../images/icons/favorite.svg';
 
-const BrowserHome = ({ reduxData }) => {
-    console.log(reduxData);
+import BrowserHomeFiltersComponent from './browserHomeFilters/browserHomeFilters.component';
+import ProductDisplayerComponent from '../../productDisplayer/productDisplayer.component';
+import SearchInput from '../../inputs/searchInput/searchInput.component';
+
+const findActiveCollection = (collectionsArray, activeCollection) => {
+    const collection = collectionsArray.find(elment => elment.shopifyId === activeCollection);
+    
+    return collection.products
+}
+
+const BrowserHome = () => {
+    const query = useStaticQuery(graphql`
+        query browserHomeQuery {
+            allShopifyCollection {
+                nodes {
+                    title
+                    shopifyId
+                    products {
+                        priceRange {
+                            minVariantPrice {
+                                amount
+                            }
+                        }
+                        title
+                        shopifyId
+                        images {
+                            localFile {
+                                childImageSharp {
+                                    gatsbyImageData
+                                }
+                            }
+                        }
+                    }
+                    image {
+                        localFile {
+                            childrenImageSharp {
+                                gatsbyImageData
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `);
+    const [ activeCollection, setActiveCollection ] = useState(query.allShopifyCollection.nodes[0].shopifyId);
+
+    const setToActiveCollection = useCallback((shopifyId) => {
+        setActiveCollection(shopifyId)
+    },[]);
 
     return (
-        <div>
-            browser view
+        <div className={ classes.browserHome }>
+            <div className={ classes.inputsBar }>
+                <BrowserHomeFiltersComponent dataArray={query.allShopifyCollection.nodes} activeCollection={activeCollection} fn={setToActiveCollection} />
+                <div className={ classes.buttons }>
+                    <SearchInput />
+                    <Link to='/favorites'>
+                        <button className={ classes.favoratesButton }>
+                            <img src={likeIcon} alt="link" />
+                        </button>
+                    </Link>
+                </div>
+            </div>
+            <ProductDisplayerComponent dataArr={findActiveCollection( query.allShopifyCollection.nodes, activeCollection )} />
         </div>
     );
 };
 
-const mapStateToProps = state => ({
-    reduxData: state
-})
 
-export default connect(mapStateToProps, null)(BrowserHome);
-// export default BrowserHome;
+export default BrowserHome;
